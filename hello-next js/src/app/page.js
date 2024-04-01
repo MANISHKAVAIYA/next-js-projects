361f85a5d6f0820acf73c9409f02aba7515b3095
mr.mk1950 <153270610+MANISHKAVAIYA@users.noreply.github.com>
2024-04-01 15:55:15 +0530

    next-js

---
+++ hello-next js/src/app/page.js
@@ -0,0 +1,10 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1>Jay Dwarkadhish</h1>
+      <h2>Manish Kavaiya</h2>
+    </main>
+  );
+}
